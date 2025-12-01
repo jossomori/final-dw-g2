8bec25e9b10e8c3c0cc541c383a5ceaa419d6d33
Josué Sosa <05josuesosa@gmail.com>
2025-11-30 21:04:44 -0300

    refactor: update initialization module to use localhost backend

--- js/init.js
+++ js/init.js
@@ -1,12 +1,12 @@
 import { fetchWithLoader } from './loader.js';
 
-export const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
-export const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
-export const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
-export const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
-export const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
-export const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
-export const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
+export const CATEGORIES_URL = "http://localhost:3000/json/cats/cat.json";
+export const PUBLISH_PRODUCT_URL = "http://localhost:3000/json/sell/publish.json";
+export const PRODUCTS_URL = "http://localhost:3000/json/cats_products/";
+export const PRODUCT_INFO_URL = "http://localhost:3000/json/products/";
+export const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/json/products_comments/";
+export const CART_INFO_URL = "http://localhost:3000/json/user_cart/";
+export const CART_BUY_URL = "http://localhost:3000/json/cart/buy.json";
 export const EXT_TYPE = ".json";
 
 export function getJSONData(url) {
